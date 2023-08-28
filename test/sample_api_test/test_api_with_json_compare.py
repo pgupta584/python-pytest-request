0d1c7400a53c4895e1f672e66d32f09ac0275bdf
pankaj.gup <pankajgup@zeta.tech>
2023-08-28 22:51:05 +0530

    Added Json extract & json compare for best Assertion

---
+++ test/sample_api_test/test_api_with_json_compare.py
@@ -0,0 +1,25 @@
+import requests
+
+from utils.common_utils import CommonUtils
+from utils.json_compare import json_compare_schema
+
+
+class TestGetAPI:
+
+    # Generic Request we can make Any HTTP method Api calls
+    @staticmethod
+    def test_get_api_read():
+        response = requests.get("https://reqres.in/api/users?page=2")
+        print("Response --> ", response.json())
+        # Verify Json Schema
+        json_compare_schema(actual=response.json(),
+                            expected="/Users/pankaj.gup/Documents/MYGIT/Python-Pytest-Request/python-pytest-request/requests_responses/Responses/reqres/user_res.json")
+        # # Reading JSON Object - use Json formatter to read manually & understand
+        # # Read Total Page & Email
+        # total_page = response.json()['total_pages']
+        # print("total_page --> ", total_page)
+        # email = response.json()['data'][0]['email']
+        # print("email -- > ", email)
+        # Extract Json key directly using utility
+        email = CommonUtils.json_extract(response.json(), "email")
+        print("email", email)
